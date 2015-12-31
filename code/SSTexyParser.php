123781c92489f66c656a35c123b830038c55cb9d
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:53:29 +1300

    Converted to PSR-2

--- code/SSTexyParser.php
+++ code/SSTexyParser.php
@@ -4,69 +4,64 @@ require_once(BASE_PATH.'/vendor/autoload.php');
 
 class SSTexyParser extends TextParser
 {
-	/** @var Texy */
-	protected $texy;
+    /** @var Texy */
+    protected $texy;
 
-	protected static $ALLOWED_TAGS_CONFIG = array(
-			'all' => Texy::ALL,
-			'none' => Texy::NONE
-		);
+    protected static $ALLOWED_TAGS_CONFIG = array(
+            'all' => Texy::ALL,
+            'none' => Texy::NONE
+        );
 
-	public function __construct($content = "")
-	{
-		Object::__construct(); //FIXME: This should be done by TextParser
-		parent::__construct($content);
+    public function __construct($content = "")
+    {
+        Object::__construct(); //FIXME: This should be done by TextParser
+        parent::__construct($content);
 
-		$this->texy = new SS_Texy();
-		$this->texy->setOutputMode(Texy::HTML5);
+        $this->texy = new SS_Texy();
+        $this->texy->setOutputMode(Texy::HTML5);
 
-		$this->setAllowedTags($this->config()->get('allowed_tags'));
+        $this->setAllowedTags($this->config()->get('allowed_tags'));
 
-		$this->texy->headingModule->top = $this->config()->get('top_heading');
+        $this->texy->headingModule->top = $this->config()->get('top_heading');
+    }
 
-	}
+    public function setAllowedTags($tags)
+    {
+        if (array_key_exists($tags, self::$ALLOWED_TAGS_CONFIG)) {
+            $this->texy->allowedTags = self::$ALLOWED_TAGS_CONFIG[$tags];
+        } else {
+            $this->texy->allowedTags = $tags;
+        }
+    }
 
-	public function setAllowedTags($tags) {
-		if(array_key_exists($tags, self::$ALLOWED_TAGS_CONFIG))
-		{
-			$this->texy->allowedTags = self::$ALLOWED_TAGS_CONFIG[$tags];
-		}
-		else
-		{
-			$this->texy->allowedTags = $tags;
-		}
-	}
+    public function getTexy()
+    {
+        return $this->texy;
+    }
 
-	public function getTexy()
-	{
-		return $this->texy;
-	}
+    public function parse()
+    {
+        return $this->texy->process($this->content);
+    }
 
-	public function parse()
-	{
-		return $this->texy->process($this->content);
-	}
+    public function typo()
+    {
+        return $this->texy->processTypo($this->content);
+    }
 
-	public function typo()
-	{
-		return $this->texy->processTypo($this->content);
-	}
+    public function disable()
+    {
+        $args = func_get_args();
+        foreach ($args as $what) {
+            $this->texy->allowed[$what] = false;
+        }
+    }
 
-	public function disable()
-	{
-		$args = func_get_args();
-		foreach($args as $what)
-		{
-			$this->texy->allowed[$what] = false;
-		}
-	}
-
-	public function enable()
-	{
-		$args = func_get_args();
-		foreach($args as $what)
-		{
-			$this->texy->allowed[$what] = true;
-		}
-	}
+    public function enable()
+    {
+        $args = func_get_args();
+        foreach ($args as $what) {
+            $this->texy->allowed[$what] = true;
+        }
+    }
 }
